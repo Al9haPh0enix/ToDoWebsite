c76921a9352de496537fde3f7444a9c6799615b3
Al9haPh0enix <maxwellhammersmark@gmail.com>
2021-08-02 06:53:32 -0700

    Working!

--- Things To Do/index.js
+++ Things To Do/index.js
@@ -1,16 +1,34 @@
 let JSONvariables;
 console.log("Startup")
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
 var todo = `
 {
-    "Watch_A_Show": ["Trollhunters", "Star Wars: Clone Wars", "Spirit", "The Mysterious Benedict Society", "Gravity Falls"],
-    "Watch_A_Movie": ["Spiderman: Homecoming", "Moana", "Luca", "Mulan", "Captain America: The First Avenger", "Avengers"],
-    "Play_A_Board_Game": ["Trouble", "Tak", "Chess", "Uno"],
-    "Play_A_Video_Game": ["Minecraft", "Portal", "Roblox", "Fortnite", "Terraria"],
-    "Play_A_Game": ["Hide And Seek", "Tag"]
+    "Watch A Show": ["Trollhunters", "Star Wars: Clone Wars", "Spirit", "The Mysterious Benedict Society"],
+    "Watch A Movie": ["Spiderman: Homecoming", "Moana", "Luca", "Mulan", "Captain America: The First Avenger", "Avengers"],
+    "Play A Board Game": ["Trouble", "Tak", "Chess", "Uno"],
+    "Play A Video Game": ["Minecraft", "Portal", "Roblox", "Fortnite", "Terraria"],
+    "Play A Game": ["Hide And Seek", "Tag"]
 }
 `;
 
 JSONvariables = JSON.parse(todo);
 
-console.log(JSONvariables)
+const keys = Object.keys(JSONvariables);
+console.log("keys: " + keys)
+
+rKey1 = keys[getRandomInt(4)];
+rKey2 = JSONvariables[rKey1];
+
+const one = JSONvariables[rKey1];
+console.log(one);
+const two = JSONvariables[rKey1][getRandomInt(JSONvariables[rKey1].length)];
+console.log(two);
+
+window.onload = function(){
+    document.getElementById("one").innerHTML = rKey1;
+    document.getElementById("two").innerHTML = two;
+}
